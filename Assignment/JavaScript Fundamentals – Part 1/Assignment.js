1f6c9631593de8a3747e2109733de3cdb4a77031
unknown <syedrakesh.me@gmail.com>
2021-06-15 23:56:12 +0600

    Data Types Complete - JavaScript Fundamentals – Part 1

---
+++ Assignment/JavaScript Fundamentals – Part 1/Assignment.js
@@ -0,0 +1,33 @@
+/*
+Values and Variables
+--------------------
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+*/
+let country = 'Bangladesh';
+let continent = 'Asia';
+let population = 163;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+
+/*
+Data Types
+----------
+1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console
+*/
+const isIsland = true;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
--- Assignment/JavaScript Fundamentals – Part 1/Values and Variables.js
+++
@@ -1,7 +0,0 @@
-let country = 'Bangladesh';
-let continent = 'Asia';
-let population = '163 million';
-
-console.log(country);
-console.log(continent);
-console.log(population);
