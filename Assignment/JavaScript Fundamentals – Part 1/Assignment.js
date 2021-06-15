e9f84cc60f201181fd0d4632d89a10eb5d696328
unknown <syedrakesh.me@gmail.com>
2021-06-16 00:08:55 +0600

    Type Conversion and Coercion - JavaScript Fundamentals – Part 1

--- Assignment/JavaScript Fundamentals – Part 1/Assignment.js
+++ Assignment/JavaScript Fundamentals – Part 1/Assignment.js
@@ -99,3 +99,22 @@ if (population > 33) {
 }else{
     console.log(`${country}'s population is ${33-population} million below average`);
 }
+
+
+
+/*
+Type Conversion and Coercion
+----------------------------
+1. Predict the result of these 5 operations without executing them:
+'9' - '5';
+'19' - '13' + '17';
+'19' - '13' + 17;
+'123' < 57;
+5 + 6 + '4' + 9 - 4 - 2;
+2. Execute the operations to check if you were right
+*/
+console.log('9' - '5'); // 4
+console.log('19' - '13' + '17'); // 617
+console.log('19' - '13' + 17); // 23
+console.log('123' < 57); // false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
