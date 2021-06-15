8f08222963c5b6f9050d24e8607e6e6c046ad72a
unknown <syedrakesh.me@gmail.com>
2021-06-15 18:41:54 +0600

    Coding Challenge 4 Complete - JavaScript Fundamentals – Part 1

--- Challenge/JavaScript Fundamentals – Part 1/Challenge_4.js
+++ Challenge/JavaScript Fundamentals – Part 1/Challenge_4.js
@@ -22,3 +22,7 @@ Hints:
 GOOD LUCK �
 */
 
+const bill = 430;
+
+const tip = bill >= 50 && bill <= 300 ? (15/100)*bill : (20/100)*bill;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill+tip}`);
