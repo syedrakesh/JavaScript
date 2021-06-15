fd99a2b49060af73d08b2ab8cdee75c78fd5ff1e
unknown <syedrakesh.me@gmail.com>
2021-06-15 23:59:30 +0600

    Let, Const and Var Complete - JavaScript Fundamentals – Part 1

--- Assignment/JavaScript Fundamentals – Part 1/Assignment.js
+++ Assignment/JavaScript Fundamentals – Part 1/Assignment.js
@@ -31,3 +31,18 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+
+
+/*
+let, const and var
+------------------
+1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console
+*/
+
+language = 'Bengali';
+population = 164;
