ae51e6b37427b25ab77545ae11139193d8ae1974
unknown <syedrakesh.me@gmail.com>
2021-06-17 02:15:49 +0600

    Update(Function Declaration - Function Expressions / Anynomous Function)

--- 02-Fundamentals-Part-2/Function Declarations vs Expressions - Anynomous.js
+++ 02-Fundamentals-Part-2/Function Declarations vs Expressions - Anynomous.js
@@ -11,3 +11,21 @@ const calcAge2 = function (birthYear) {
 }
 
 console.log(calcAge1(1995), calcAge2(1997));
+
+
+
+// Function Declaration can be called before the function declarae.
+const age1 = calcAge3(2004)
+function calcAge3(birthYear) {
+    return 2021 - birthYear;
+}
+
+
+
+// Function Expressions / Anynomous Function ca not be called before the function declared. First declare the function then we can call the function.
+const calcAge4 = function (birthYear) {
+    return 2021 - birthYear;
+}
+const age2 = calcAge4(2000);
+
+console.log(age1, age2);
