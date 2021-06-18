022d007e6518fdcefc2e47864255825c44f754da
unknown <syedrakesh.me@gmail.com>
2021-06-18 14:39:07 +0600

    Introduction to Objects

---
+++ 02-Fundamentals-Part-2/Introduction to Objects.js
@@ -0,0 +1,17 @@
+const rakeshArray = [
+    'Syed',
+    'Rakesh',
+    2021-1997,
+    'Software Engineer',
+    ['Faira', 'Nishat', 'Khan'],
+];
+console.log(rakeshArray);
+
+const rakeshObject = {
+    firstName: 'Syed',
+    lastName: 'Rakesh',
+    dob: 2021-1997,
+    occupation: 'Software Engineer',
+    friends: ['Faria', 'Nishat', 'Khan'],
+}
+console.log(rakeshObject);
